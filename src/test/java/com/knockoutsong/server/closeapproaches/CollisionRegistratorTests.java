f628e1a67ac996e3f916370492180cc36c808750
Danny <liquidcore7@gmail.com>
2018-10-20 17:34:09 +0300

    collision test

---
+++ src/test/java/com/knockoutsong/server/closeapproaches/CollisionRegistratorTests.java
@@ -0,0 +1,17 @@
+package com.knockoutsong.server.closeapproaches;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+
+@Suite.SuiteClasses({CollisionRegistratorTestClass.class})
+@RunWith(Suite.class)
+public class CollisionRegistratorTests {
+
+}
